4749047db2fda6720331de1e65fff984a4c09c3e
josdic1 <joshuadicker@gmail.com>
2025-01-14 10:53:03 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+
+const main = document.querySelector('main#main');
+if (main) {
+  main.remove();
+}
+
+const myName = "Josh"
+const victory= "is the champion"
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.innerHTML = `${myName.toUpperCase()} ${victory}`;
+
+document.body.append(newHeader);
